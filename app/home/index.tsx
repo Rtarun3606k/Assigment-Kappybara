1c764f6c56a0b5368338119ad792779c22e8216a
Rtarun3606k <r.tarunnayaka25042005@gmail.com>
2025-06-01 20:34:44 +0530

    feat: add TRPC and React Query dependencies, implement home page component

---
+++ app/home/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const home_Page = () => {
+  return (
+    <View>
+      <Text>home_Page</Text>
+    </View>
+  )
+}
+
+export default home_Page
